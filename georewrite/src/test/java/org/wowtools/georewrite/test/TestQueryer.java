5f101012e82f6fa9d088789ec082c2fc78db2530
刘雨 <liuyu@wowtools.org>
2016-12-28 17:43:17 +0800

    加入列对象获取器后，相应调整示例代码

--- georewrite/src/test/java/org/wowtools/georewrite/test/TestQueryer.java
+++ georewrite/src/test/java/org/wowtools/georewrite/test/TestQueryer.java
@@ -27,19 +27,19 @@ public class TestQueryer extends GeoSqlQueryer {
 	private static final int num = 10000;
 
 	private static class MyFeature {
-		int id;//字段中不要带fid，否则geoserver会在查询时重命名一个fid_1之类的出来，很麻烦
+		int id;// 字段中不要带fid，否则geoserver会在查询时重命名一个fid_1之类的出来，很麻烦
 		String name;
 		double value;
 		Point shape;
+		byte[] wkb;
 	}
 
-	private final MyFeature[] features = new MyFeature[num];
-
-	public TestQueryer(String tableName, HashMap<String, ColumnDefinition> columnInfo) {
-		super(tableName, columnInfo);
-		Random r = new Random();
+	private static final MyFeature[] features = new MyFeature[num];
+	static {
+		// 初始化时搞一批测试数据
+		Random r = new Random(233);
 		GeometryFactory gf = new GeometryFactory();
-
+		WKBWriter wr = new WKBWriter();
 		for (int i = 0; i < num; i++) {
 			MyFeature f = new MyFeature();
 			f.id = i;
@@ -48,47 +48,69 @@ public class TestQueryer extends GeoSqlQueryer {
 			Coordinate coordinate = new Coordinate(100 + r.nextDouble() * 20, 20 + r.nextDouble() * 10);
 			Point pt = gf.createPoint(coordinate);
 			f.shape = pt;
+			f.wkb = wr.write(pt);
 			features[i] = f;
 		}
+	}
+
+	public TestQueryer(String tableName, HashMap<String, ColumnDefinition> columnInfo) {
+		super(tableName, columnInfo);
 	}
 
 	@Override
 	public ResultSet query(Connection conn, String columnPart, String fun, String pg) throws SQLException {
 		SimpleResultSet rs;
-		//利用父类中的方法构造一个SimpleResultSet对象
+		// 利用父类中的方法构造一个SimpleResultSet对象
 		String[] columns = columnPart2columnArr(columnPart);
 		rs = buildSimpleResultSetByColumns(columns);
-		
-		//查询features中在范围内的数据并返回(实际运用中，我们应该建立一个索引，而非像本例子中一样遍历)
+
+		// (可选操作)构建一个对象获取器数组，提前处理if ("SHAPE".equals(columnName)之类的判断
+		ObjGetter<MyFeature>[] objGetters = feature2Row(columns);
+
+		// 查询features中在范围内的数据并返回(实际运用中，我们应该建立一个索引，而非像本例子中一样遍历)
 		Geometry extent = pg2Extent(pg);
-		WKBWriter wr = new WKBWriter();
-		for(MyFeature f:features){
-			if(extent.intersects(f.shape)){
-				rs.addRow(toRow(columns, f, wr));
+		for (MyFeature f : features) {
+			if (extent.intersects(f.shape)) {
+				Object[] row = new Object[objGetters.length];
+				for (int i = 0; i < objGetters.length; i++) {
+					row[i] = objGetters[i].get(f, -1, fun);
+				}
+				rs.addRow(row);
 			}
 		}
 		return rs;
 	}
-	
-	private Object[] toRow(String[] columns,MyFeature f,WKBWriter wr){
-		Object[] row = new Object[columns.length];
-		for(int i = 0;i<columns.length;i++){
-			String c = columns[i];
-			if("SHAPE".equals(c)){
-				byte[] wkb = wr.write(f.shape);
-				row[i] = wkb;
-			}else if("VALUE".equals(c)){
-				row[i] = f.value;
-			}else if("NAME".equals(c)){
-				row[i] = f.name;
-			}else if("TCODE".equals(c)){
-				row[i] = "TCODE";
-			}else{
-				//fid就用id好了，只要不重复都行
-				row[i] = f.id;
+
+	private ObjGetter<MyFeature>[] feature2Row(String[] columns) {
+		@SuppressWarnings("unchecked")
+		ObjGetter<MyFeature>[] objGetters = new ObjGetter[columns.length];
+		for (int i = 0; i < columns.length; i++) {
+			ObjGetter<MyFeature> getter;
+			String columnName = columns[i];
+			if ("SHAPE".equals(columnName)) {
+				getter = (f, fid, tcode) -> {
+					return f.wkb;
+				};
+			} else if ("VALUE".equals(columnName)) {
+				getter = (f, fid, tcode) -> {
+					return f.value;
+				};
+			} else if ("NAME".equals(columnName)) {
+				getter = (f, fid, tcode) -> {
+					return f.name;
+				};
+			} else if ("TCODE".equals(columnName)) {
+				getter = (f, fid, tcode) -> {
+					return tcode;
+				};
+			} else {
+				getter = (f, fid, tcode) -> {
+					return f.id;
+				};
 			}
+			objGetters[i] = getter;
 		}
-		return row;
+		return objGetters;
 	}
 
 }
